ae2929d93c29260d14f14bdb948f1cc50f43a956
birkas3000 <p-boris12@mail.ru>
2022-06-24 16:32:25 +0300

    Lab1.5

--- Lab1.5/lab1_5.py
+++ Lab1.5/lab1_5.py
@@ -10,7 +10,7 @@ for current_file in glob.glob(r'..\config_files\*.txt'):
                 str_common = (ip_line.replace("ip address", " ").strip()).split(' ')
                 str_ip = str_common[0]
                 str_mask = str_common[-1]
-                ip_adresseses.append({'IP_{}_{}'.format(i, str(current_file).split('\\')[-1]): str_ip, 'Mask_{}_{}'.format(i, str(current_file).split('\\')[-1]) : str_mask})
+                ip_adresseses.append({'IP': str_ip, 'Mask' : str_mask})
                 i += 1
 
 for i in range(len(ip_adresseses)):
