3c33f32855033795cae1d54c401207931df55276
birkas3000 <p-boris12@mail.ru>
2022-06-24 16:22:02 +0300

    Lab1.5

---
+++ Lab1.5/lab1_5.py
@@ -0,0 +1,17 @@
+import glob
+
+ip_adresseses = []
+
+for current_file in glob.glob(r'..\config_files\*.txt'):
+    with open(current_file) as f:
+        i = 0
+        for ip_line in f:
+            if ip_line.find("ip address") == 1:
+                str_common = (ip_line.replace("ip address", " ").strip()).split(' ')
+                str_ip = str_common[0]
+                str_mask = str_common[-1]
+                ip_adresseses.append({'IP': str_ip, 'Mask' : str_mask})
+                i += 1
+
+for i in range(len(ip_adresseses)):
+    print(ip_adresseses[i])
